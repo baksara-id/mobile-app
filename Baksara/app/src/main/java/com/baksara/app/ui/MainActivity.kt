93e8a917e663fafd5927fb50f3b2a738168d3a38
Snahfu <hanxin131@gmail.com>
2023-05-13 22:47:01 +0700

    bottom navigation done

--- Baksara/app/src/main/java/com/baksara/app/ui/MainActivity.kt
+++ Baksara/app/src/main/java/com/baksara/app/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.baksara.app
+package com.baksara.app.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.baksara.app.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
