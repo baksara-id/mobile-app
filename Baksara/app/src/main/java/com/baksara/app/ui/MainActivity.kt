8fd3a989897ed60f5fe8b69df02c6f602e0ecfaf
Snahfu <hanxin131@gmail.com>
2023-05-18 10:19:50 +0700

    setup untuk bottom nav

--- Baksara/app/src/main/java/com/baksara/app/ui/MainActivity.kt
+++ Baksara/app/src/main/java/com/baksara/app/ui/MainActivity.kt
@@ -2,11 +2,35 @@ package com.baksara.app.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.baksara.app.R
+import com.baksara.app.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        bottomNavigationSetup()
+    }
+
+    private fun bottomNavigationSetup(){
+        val navView: BottomNavigationView = findViewById(R.id.bottom_nav)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        val appBarConfiguration = AppBarConfiguration.Builder(
+            R.id.navigation_kelas, R.id.navigation_pustaka, R.id.navigation_scanner, R.id.navigation_berita, R.id.navigation_profil
+        ).build()
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
