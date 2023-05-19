6e8fb22a36858572945d146b91e800e2b4827c3d
ToastedBaguette <ronyhartonoirawan@gmail.com>
2023-05-19 21:15:44 +0700

    Onboarding Done

--- Baksara/app/src/main/java/com/baksara/app/ui/OnBoardingActivity.kt
+++ Baksara/app/src/main/java/com/baksara/app/ui/OnBoardingActivity.kt
@@ -1,12 +1,29 @@
 package com.baksara.app.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.baksara.app.R
+import com.baksara.app.databinding.ActivityOnboardingBinding
 
-class OnBoardingActivity : AppCompatActivity() {
+class OnBoardingActivity : AppCompatActivity(){
+    private lateinit var binding: ActivityOnboardingBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboarding)
+        supportActionBar?.hide()
+
+        binding = ActivityOnboardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.btnMulai.setOnClickListener {
+                binding.btnMulai.isEnabled = false
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+        }
+
+
     }
 }
--- Baksara/app/src/main/java/com/baksara/app/ui/SplashActivity.kt
+++ Baksara/app/src/main/java/com/baksara/app/ui/SplashActivity.kt
@@ -1,12 +1,25 @@
 package com.baksara.app.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.baksara.app.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
+
+        val delay = 4000.0.toLong()
+
+        val intent = Intent(this, OnBoardingActivity::class.java)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(intent)
+            finish()
+        }, delay)
     }
 }
